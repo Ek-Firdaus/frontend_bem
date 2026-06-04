b59d07c4ce6566434ed7c7129905a39415602b3d
Muhammad Firdaus <egoekosetio@gmail.com>
2026-06-04 16:39:16 +0800

    fix: url backend

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'env.VITE_API_URL',
+        target: 'https://bem-backend-production.up.railway.app',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
